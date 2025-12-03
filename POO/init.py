70b2f65859ad2dcd92285f1c3d58bcdb2659bc2c
projeto novo <caioosilva618@gmail.com>
2025-12-02 21:28:01 -0400

    OPP

--- POO/init.py
+++ POO/init.py
@@ -1,11 +1,51 @@
 
-class Livro:
-    # o Montador da fábrica (contrutor)
+# class Livro:
+#     # o Montador da fábrica (contrutor)
+#     def __init__(self, titulo_dado, autor_dado):
+#         # A magia acontece aqui, os atributos são iniciados
+#         self.titulo = titulo_dado
+#         self.autor = autor_dado
+#
+# livro_1 = Livro("Interistellar", "Guilherme")
+# print(livro_1.titulo)
+
+# com input´s
+
+class Livros:
+
     def __init__(self, titulo_dado, autor_dado):
-        # A magia acontece aqui, os atributos são iniciados
         self.titulo = titulo_dado
         self.autor = autor_dado
 
-livro_1 = Livro("Interistellar", "Guilherme")
-print(livro_1.titulo)
+    def exibir_info(self):
+        print(f"Livro Cadastrado: {self.titulo} - {self.autor}")
 
+print("-"*40)
+print(" Cadastro de novo livro (1/2) ")
+print("-"*40)
+
+titulo_a = input("Digite o Titulo do Livro A:")
+autor_a = input("Digite o Autor do Livro A:")
+
+
+livro_a = Livros(titulo_a, autor_a)
+
+print("-"*40)
+print(" Cadastro de novo livro (2/2) ")
+print("-"*40)
+
+
+titulo_b = input("Digite o Titulo do Livro B:")
+autor_b = input("Digite o Autor do Livro B:")
+livro_b = Livros(titulo_b, autor_b)
+
+print("\n" + "-"*40)
+print("Verificação final: o Poder do 'SELF'")
+print("-"*40)
+
+livro_a.exibir_info()
+livro_b.exibir_info()
+
+print("\n  --- Acessando atributos Diretamente---")
+print(f"titulo no Objeto A: {livro_a.titulo}")
+print(f"titulo no Objeto B: {livro_b.titulo}")
